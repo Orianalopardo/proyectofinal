0fd6245981ef5c0ff203cc0c4a4daa3ec1e93528
Orianalopardo <orilopardo123@gmail.com>
2023-08-09 12:30:14 -0300

    mipagina

--- app.js
+++ app.js
@@ -10,52 +10,50 @@ let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 const getProducts = async ()=> {
   const response = await fetch ("data.json");
   const data = await response.json();
-  console.log(data);
+  data.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+      <img src="${product.img}">
+      <h3>${product.nombre}</h3>
+      <p class="price">${product.precio} $</p>
+    `;
+  
+    shopContent.append(content);
+  
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
+  
+    content.append(comprar);
+  
+    comprar.addEventListener("click", () => {
+      const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+  
+      if (repeat) {
+        carrito.map((prod) => {
+          if (prod.id === product.id) {
+            prod.cantidad++;
+          }
+        });
+      } else {
+        carrito.push({
+          id: product.id,
+          img: product.img,
+          nombre: product.nombre,
+          precio: product.precio,
+          cantidad: product.cantidad,
+        });
+        console.log(carrito);
+        console.log(carrito.length);
+        carritoCounter();
+        saveLocal();
+      }
+    });
+  });
+  
 };
 getProducts();
-
-
-productos.forEach((product) => {
-  let content = document.createElement("div");
-  content.className = "card";
-  content.innerHTML = `
-    <img src="${product.img}">
-    <h3>${product.nombre}</h3>
-    <p class="price">${product.precio} $</p>
-  `;
-
-  shopContent.append(content);
-
-  let comprar = document.createElement("button");
-  comprar.innerText = "comprar";
-  comprar.className = "comprar";
-
-  content.append(comprar);
-
-  comprar.addEventListener("click", () => {
-    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
-
-    if (repeat) {
-      carrito.map((prod) => {
-        if (prod.id === product.id) {
-          prod.cantidad++;
-        }
-      });
-    } else {
-      carrito.push({
-        id: product.id,
-        img: product.img,
-        nombre: product.nombre,
-        precio: product.precio,
-        cantidad: product.cantidad,
-      });
-      console.log(carrito);
-      console.log(carrito.length);
-      carritoCounter();
-      saveLocal();
-    }
-  });
-});
 
 //set item
 const saveLocal = () => {
